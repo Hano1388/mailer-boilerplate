c412bcc501e4b0a379fbfd7acdb8c283127b462f
Hano <hinsul3113@gmail.com>
2019-01-24 23:11:03 -0800

    added nodemailer to server.js and post endpoint

--- server.js
+++ server.js
@@ -8,6 +8,53 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
+app.post('/', function(req, res) {
+  const output = `
+    <p>You have a new contact request</p>
+    <h3>contact details</h3>
+    <ul>
+      <li>Name: ${req.body.name}</li>
+      <li>Company: ${req.body.company}</li>
+      <li>Email: ${req.body.email}</li>
+      <li>Phone: ${req.body.phone}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.message}</p>
+  `;
+  // create reusable transporter object using the default SMTP transport
+  var transporter = nodemailer.createTransport({
+    service: 'webmail',
+    host: 'mail.bcts.ca',
+    port: 465,
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  console.log("req.body.email: ", req.body.email);
+
+  // setup email data with unicode symbol
+  var mailOptions = {
+    from: `nodemailer contact ${req.body.email}`,
+    to: 'hindreen@bcts.ca',
+    subject: 'User Form Contact',
+    html: output
+  };
+
+  transporter.sendMail(mailOptions, function(error, info) {
+    if (error) {
+      return console.log(error);
+    }
+
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+    res.send({
+      msg: 'Email has been sent!'
+    });
+  });
+});
+
 app.listen(PORT, function() {
   console.log('Server listening on port ' + PORT);
 });
